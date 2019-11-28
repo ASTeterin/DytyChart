43fcde7190d52cd9b0b26a27509d4c11d73988b7
ASTeterin <at28813905@gmail.com>
2019-11-28 22:44:47 +0300

    добавил маршрутизацию. Работает

--- wwwroot/dist/polyfills.js
+++ wwwroot/dist/polyfills.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "39c7cd567f881a5ca466";
+/******/ 	var hotCurrentHash = "a26201f77d520041bab2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
