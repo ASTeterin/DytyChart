0164c2eeb4769764ed08df78ef62fd2cc8c4c30a
ASTeterin <at28813905@gmail.com>
2020-01-06 12:05:45 +0300

    подсчет количества свободных слотов у сотрудников

---
+++ Dto/WorkerDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dutyChart.Dto
+{
+    public class WorkerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int IdGroup { get; set; }
+        public int countFreeSlots { get; set; }
+    }
+}
--- controllers/WorkerController.cs
+++ controllers/WorkerController.cs
@@ -2,6 +2,9 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using dutyChart.Models;
+using dutyChart.Dto;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 
 
@@ -20,6 +23,61 @@ namespace dutyChart.Controllers
         public IEnumerable<Worker> Get()
         {
             return db.Workers.ToList();
+        }
+
+        private int  getFreeSlots(int groupId, int countUsedSlots)
+        {
+            var countFreeSlots = 0;
+            switch (groupId)
+            {
+                case 1:
+                    countFreeSlots = 1 - countUsedSlots;
+                    break;
+                case 2:
+                    countFreeSlots = 1 - countUsedSlots;
+                    break;
+                case 3:
+                    countFreeSlots = 5 - countUsedSlots;
+                    break;
+            }
+            return countFreeSlots;
+        }
+
+        [HttpGet, Route("get-wirker-in-day")]
+        public IEnumerable<WorkerDto> GetWorkerDtoInDay(DateTime date)
+        {
+            List <WorkerDto> workersDto= new List<WorkerDto> { };
+            var workers = db.Workers.ToList();
+            var hId = db.Hours.Where(h => h.Date == date).Select(h => h.Id)
+                .ToList();
+            var hours = db.Hours
+                .Include(Hour.SlotsProperty)
+                .Where(h => h.Date == date)
+                .ToList();
+            var coutUsedSlots = db.Slots.Where(s => hId.Contains(s.HourId)).GroupBy(s => s.WorkerId).ToList();
+            var result = coutUsedSlots.Join(workers, us => us.Key, w => w.Id, (us, w) => new {Count=us.Key,  WorkerId = w.Id, GroupId = w.IdGroup, Name = w.Name});
+            foreach (var us in coutUsedSlots)
+            {
+                foreach (var w in workers)
+                {
+                    if (us.Key == w.Id)
+                    {
+                        int countFreeSlots = getFreeSlots(w.IdGroup, us.Count());
+                        if (countFreeSlots > 0)
+                        { 
+                            WorkerDto wDto = new WorkerDto();
+                            wDto.Id = w.Id;
+                            wDto.IdGroup = w.IdGroup;
+                            wDto.Name = w.Name;
+                            wDto.countFreeSlots = countFreeSlots;
+                            //wDto.countFreeSlots = (w.IsDuty) ? 0 : getFreeSlots(w.IdGroup, us.Count());
+                            workersDto.Add(wDto);
+                            break;
+                        }
+                    }
+                }
+            }
+            return workersDto;
         }
 
         [HttpGet("{id}")]
@@ -64,6 +122,7 @@ namespace dutyChart.Controllers
             }
             return Ok(worker);
         }
+
 
         
     }
